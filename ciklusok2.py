4ffbd409de4f2514cc0bc18dd2f3b836e7a8b177
balazsheitler-del <balazsheitler@gmail.com>
2025-09-12 13:00:19 +0200

    while done

---
+++ ciklusok2.py
@@ -0,0 +1,10 @@
+# kerjunk be szamokat es adjuk meg az osszeget
+# 1 0 vegjelig
+# 2 enter vegjelig hf : ugyan ez az algoritmust!
+osszeg=0
+be_n=1
+while be_n !=0:
+    be_n =int(input("kereke egy szamot"))
+    osszeg+be_n
+
+print(osszeg)
